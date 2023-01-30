21a4781de802299c241c00378eab2836af417395
Darren Massey <phailhaus@gmail.com>
2023-01-30 12:46:17 -0600

    Fuel! A fuel meter! And a lander class!

--- Terminal UI/lander.py
+++ Terminal UI/lander.py
@@ -27,7 +27,6 @@ screenwidth, screenheight = termwidth * fontsize[0], termheight * fontsize [1]
 newline = chr(10)
 blank = " "
 fullblock = chr(9608)
-lowerhalfblock = chr(9604)
 triangles = str( # Eight directions starting from up and going clockwise
 	chr(9650) +
 	chr(9701) +
@@ -37,6 +36,17 @@ triangles = str( # Eight directions starting from up and going clockwise
 	chr(9699) +
 	chr(9664) +
 	chr(9700)
+)
+
+lowerblocks = str( # Eight block sizes ascending from 1/8th to full
+	chr(9601) +
+	chr(9602) +
+	chr(9603) +
+	chr(9604) +
+	chr(9605) +
+	chr(9606) +
+	chr(9607) +
+	chr(9608)
 )
 
 def add_environment(space):
@@ -78,16 +88,55 @@ def write_landerstats(screen, lander):
 	landerangle = math.degrees(lander.body.angle)
 	landerav = math.degrees(lander.body.angular_velocity)
 	landervel = lander.body.velocity
-	line = 2
-	screen.write_string(f"Angle: {round(landerangle)}{chr(176)}", (2,line))
-	line += 1
-	screen.write_string(f"A Vel: {round(landerav)}/sec", (2,line))
-	line += 1
-	screen.write_string(f"X Vel: {round(landervel[0])}", (2,line))
-	line += 1
-	screen.write_string(f"Y Vel: {round(landervel[1])}", (2,line))
 
+	line = 2
+	screen.write_string(f"Angle: {round(landerangle)}{chr(176)}", (4,line))
+	line += 1
+	screen.write_string(f"A Vel: {round(landerav)}/sec", (4,line))
+	line += 1
+	screen.write_string(f"X Vel: {round(landervel[0])}", (4,line))
+	line += 1
+	screen.write_string(f"Y Vel: {round(landervel[1])}", (4,line))
 	
+	screen.write_single("F", (2, 2))
+	barlength = 3
+	landerfuelpercent = lander.fuel/lander.startingfuel
+	barremainder, barblocks = math.modf(landerfuelpercent * barlength)
+	barstring = fullblock * int(barblocks)
+	barremainder = round(barremainder * 8-1)
+	if barremainder != -1:
+		barstring += lowerblocks[barremainder]
+	barcolor = None
+	if landerfuelpercent <= 2/3: barcolor = (230, 230, 50)
+	if landerfuelpercent <= 1/3: barcolor = (255, 50, 50)
+	screen.write_string(barstring, (2, 5), -90, fgcolor=barcolor)
+
+
+class Lander:
+	def __init__(self, space):
+		self.space = space
+		self.body = pymunk.Body()
+		self.shape = pymunk.Poly.create_box(body=self.body, size=(fontsize[0] * 4, fontsize[1] * 4), radius=1)
+		self.shape.density = 1
+		self.space.add(self.body, self.shape)
+		self.body.position = (screenwidth/2, 10)
+		self.startingfuel = 1
+		self.fuel = self.startingfuel
+		self.defaultpower = 600
+	
+	def fire_engine(self, direction=(0,-1), power=None):
+		if self.fuel <= 0:
+			return
+		if power==None:
+			power = self.defaultpower
+		powerratio = power/self.defaultpower
+		direction = pymunk.Vec2d(direction[0], direction[1]).normalized()
+		if direction == 0:
+			return
+		self.body.apply_force_at_local_point((power * direction.x * self.body.mass, power * direction.y * self.body.mass))
+		self.fuel -= .5 * powerratio / framerate
+
+
 
 def main():
 	pygameScreen = pygame.display.set_mode((termwidth * fontsize[0], termheight * fontsize[1]))
@@ -96,11 +145,7 @@ def main():
 	space = pymunk.Space()
 	space.gravity = (0.0, 200.0)
 	lines = add_environment(space)
-	landerbody = pymunk.Body()
-	lander = pymunk.Poly.create_box(body=landerbody, size=(fontsize[0] * 4, fontsize[1] * 4), radius=1)
-	lander.density = 1
-	space.add(landerbody, lander)
-	landerbody.position = (screenwidth/2, 10)
+	lander = Lander(space)
 
 	framenum = 0
 
@@ -112,11 +157,11 @@ def main():
 						sys.exit(0)
 		
 		keyspressed = pygame.key.get_pressed()
-		if keyspressed[K_UP] or keyspressed[K_KP8]: landerbody.apply_force_at_local_point((0,-600 * landerbody.mass))
-		if keyspressed[K_KP7]: landerbody.apply_force_at_local_point((-600 * landerbody.mass, 0))
-		if keyspressed[K_KP9]: landerbody.apply_force_at_local_point((600 * landerbody.mass, 0))
-		if keyspressed[K_LEFT] or keyspressed[K_KP4]: landerbody.angular_velocity -= .01
-		if keyspressed[K_RIGHT] or keyspressed[K_KP6]: landerbody.angular_velocity += .01
+		if keyspressed[K_UP] or keyspressed[K_KP8]: lander.fire_engine()
+		if keyspressed[K_KP7]: lander.fire_engine((-1, 0))
+		if keyspressed[K_KP9]: lander.fire_engine((1, 0))
+		if keyspressed[K_LEFT] or keyspressed[K_KP4]: lander.body.angular_velocity -= .01
+		if keyspressed[K_RIGHT] or keyspressed[K_KP6]: lander.body.angular_velocity += .01
 
 		space.step(1/framerate)
 		mainscreen.reset()
@@ -124,7 +169,7 @@ def main():
 		mainscreen.write_rectangle(fullblock, (0, 0), termwidth, termheight, fgcolor = (100, 255, 100), bgcolor = (30, 80, 30))
 		uiscreen.write_rectangle(fullblock, (0, 0), uiscreen.width, uiscreen.height)
 		write_lines(mainscreen, lines, fullblock)
-		write_lander(mainscreen, lander)
+		write_lander(mainscreen, lander.shape)
 		write_landerstats(uiscreen, lander)
 		mainscreen.write_surface(uiscreen, (0,0))
 
