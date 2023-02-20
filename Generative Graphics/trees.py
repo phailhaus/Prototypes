cde102d8a9b3e6dcc2f8de20770af1b39c0031b6
Darren Massey <phailhaus@gmail.com>
2023-02-19 20:17:59 -0600

    Improved wind and made a monster

--- Generative Graphics/trees.py
+++ Generative Graphics/trees.py
@@ -97,18 +97,72 @@ class Tree:
 						self.generate(childbranch, 0)
 	
 	# Runs self.drawbranch on self.tree
-	def draw(self, surface = pygame.surface, position = tuple, color = (255, 255, 255), angle = 0):
-		self.drawbranch(self.tree, surface, position, color, angle)
+	def draw(self, surface = pygame.surface, position = tuple, color = (255, 255, 255), velocity = pygame.math.Vector2(0,0)):
+		self.drawbranch(self.tree, surface, position, color, velocity)
 	
 	# Recursively draws the tree at the specified location. Bends it based on the provided angle to simulate wind. The Y coordinate is flipped when drawing.
-	def drawbranch(self, branch = Branch, surface = pygame.surface, position = tuple, color = (255, 255, 255), angle = 0):
+	def drawbranch(self, branch = Branch, surface = pygame.surface, position = tuple, color = (255, 255, 255), velocity = pygame.math.Vector2(0,0)):
 		start_pos = pygame.math.Vector2(position)
-		reflectedbranchvector = pygame.math.Vector2((branch.vector.x, -branch.vector.y)) # Flip Y coordinate
-		reflectedbranchvector.rotate_ip(angle * ((branch.layer + 1.5) / 1.5) * branch.windimpact) # Apply wind
+		branchvector = pygame.math.Vector2(branch.vector)
+		branchvectorlength = branchvector.length()
+		branchvector += velocity * branch.windimpact
+		branchvector.scale_to_length(branchvectorlength)
+		reflectedbranchvector = pygame.math.Vector2((branchvector.x, -branchvector.y)) # Flip Y coordinate
 		end_pos = start_pos + reflectedbranchvector
 		pygame.draw.aaline(surface, color, start_pos, end_pos)
 		for childbranch in branch.children:
-			self.drawbranch(childbranch, surface, end_pos, color, angle = angle)
+			self.drawbranch(childbranch, surface, end_pos, color, velocity = velocity)
+
+class PlayerTree:
+	def __init__(self, tree, player_pos=pygame.math.Vector2(0, 0)):
+		self.tree = tree
+		self.gravity = 1.5
+		self.player_speed = 20
+		self.player_jump = 40
+		self.player_vel = pygame.math.Vector2(0, 0)
+		self.player_pos = player_pos
+		self.combined_vel_last = pygame.math.Vector2(0, 0)
+		self.jumping = False
+
+	def moveleft(self, stop=False):
+		if stop:
+			self.player_vel.x += self.player_speed
+		else:
+			self.player_vel.x -= self.player_speed
+
+	def moveright(self, stop=False):
+		if stop:
+			self.player_vel.x -= self.player_speed
+		else:
+			self.player_vel.x += self.player_speed
+	
+	def jump(self):
+		if not self.jumping:
+			self.player_vel.y -= self.player_jump
+			self.jumping = True
+	
+	def update(self):
+		self.player_vel.y += self.gravity
+		self.player_pos += self.player_vel
+		if self.player_pos.y >= screen_height: # Land on ground
+			self.player_pos.y = screen_height-1
+			self.player_vel.y = 0
+			self.jumping = False
+		if self.player_pos.x < 0: # Clamp to screen left
+			self.player_pos.x = 0
+			self.player_vel.x = 0
+		if self.player_pos.x >= screen_width: # Clamp to screen right
+			self.player_pos.x = screen_width-1
+			self.player_vel.x = 0
+
+	def draw(self, screen):
+		global windvelocity
+		combined_vel = pygame.math.Vector2(-self.player_vel.x, self.player_vel.y)
+		combined_vel += windvelocity
+		combined_vel += self.combined_vel_last
+		combined_vel /= 2
+		self.combined_vel_last = combined_vel
+		self.tree.draw(screen, self.player_pos, velocity=combined_vel)
 
 trees = list()
 sparsefractaltree = Tree()
@@ -183,8 +237,11 @@ regulartree.breakafterchance = 0 # Chance for a child to have no children of its
 for tree in trees:
 	tree.startgeneration()
 
-bendangle = 0
-targetbendangle = 30
+player = PlayerTree(skinnytree, pygame.math.Vector2(screen_width/2, screen_height))
+
+wind = 0
+targetwind = 0
+windmax = 25
 
 # Game loop
 running = True
@@ -201,20 +258,47 @@ while running:
 			if event.key == pygame.K_SPACE:
 				for tree in trees:
 					tree.startgeneration()
+			elif event.key == pygame.K_1:
+				player.tree = trees[0]
+			elif event.key == pygame.K_2:
+				player.tree = trees[1]
+			elif event.key == pygame.K_3:
+				player.tree = trees[2]
+			elif event.key == pygame.K_4:
+				player.tree = trees[3]
+			elif event.key == pygame.K_LEFT:
+				player.moveleft()
+			elif event.key == pygame.K_RIGHT:
+				player.moveright()
+			elif event.key == pygame.K_UP:
+				player.jump()
+		elif event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT:
+				player.moveleft(stop = True)
+			elif event.key == pygame.K_RIGHT:
+				player.moveright(stop = True)
 
 	# Clear the screen
 	screen.fill((0, 0, 0))
 
 	if frame % 15 == 0:
-		targetbendangle += random.randint(-15, 15)
-		if targetbendangle > 30: targetbendangle = 15
-		elif targetbendangle < -30: targetbendangle = -15
+		targetwind += (random.random() * windmax * 2) - windmax
+		if targetwind > windmax: targetwind = windmax/2
+		elif targetwind < -windmax: targetwind = -windmax/2
 	
-	bendangle += (targetbendangle - bendangle) / 60
+	wind += (targetwind - wind) / 60
+	windvelocity = pygame.math.Vector2(wind, wind/3)
+	print(windvelocity)
 
 	# Draw the trees
 	for i in range(len(trees)):
-		trees[i].draw(screen, ((((screen_width * .66) / (len(trees)-1)) * i) + (screen_width * .1666), screen_height - 10), angle=bendangle)
+		trees[i].draw(screen, ((((screen_width * .66) / (len(trees)-1)) * i) + (screen_width * .1666), screen_height - 10), velocity=windvelocity)
+
+	# Update the player
+	player.update()
+
+	# Draw the player
+	player.draw(screen)
 
 	# Update the screen
 	pygame.display.flip()
