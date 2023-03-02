bd1d444c686eba23e65ffc9f32b62d2751363f97
Darren Massey <phailhaus@gmail.com>
2023-03-01 22:20:32 -0600

    Started moving to a more universal system

--- LocalGrids/localgrids.py
+++ LocalGrids/localgrids.py
@@ -16,6 +16,70 @@ screensize = (screenwidth, screenheight)
 
 framerate = 60
 
+class Localgrid:
+	def __init__(self, parentbody, defaultcolor = (255, 255, 255)):
+		self.parentbody = parentbody
+		self.defaultcolor = defaultcolor
+		self.space = pymunk.Space()
+		self.bodies = list()
+		self.shapes = list()		
+	
+	def addBodies(self, *bodies):
+		for body in bodies:
+			self.bodies.append(body)
+			self.space.add(body)
+	
+	def addShapes(self, *shapes):
+		for shape in shapes:
+			try: shape.hidden
+			except AttributeError: shape.hidden = False
+
+			try: shape.color
+			except AttributeError: shape.color = self.defaultcolor
+
+			self.shapes.append(shape)
+			self.space.add(shape)
+
+		
+	
+	def prestep(self):
+		for body in self.bodies:
+			if body.body_type == pymunk.Body.DYNAMIC:
+				body.prestepvelocity = self.parentbody.velocity_at_local_point(body.position) # Record absolute velocity of the parent object at the location of the child object
+				body.prestepangvelocity = self.parentbody.angular_velocity # Record angular velocity of the parent object
+	
+	def poststep(self):
+		for body in self.bodies:
+			if body.body_type == pymunk.Body.DYNAMIC:
+				body.poststepvelocity = self.parentbody.velocity_at_local_point(body.position) # Record absolute velocity again after the timestep so that acceleration can be computed
+				body.poststepangvelocity = self.parentbody.angular_velocity # Record angular velocity again so that angular acceleration can be computed
+
+				body.acceleration = (body.poststepvelocity - body.prestepvelocity).rotated(-self.parentbody.angle + math.radians(180)) # Calculate acceleration of the parent, rotate it by parent angle to translate it into child space, and flip it so that the child gets the opposite acceleration
+				body.velocityadd = body.acceleration + (self.parentbody.space.gravity.rotated(-self.parentbody.angle)/framerate) # Add gravity to the acceleration. If parent is in freefall, gravity on the parent and this gravity will cancel out, providing weightlessness.
+				body.velocity += body.velocityadd # Apply acceleration/gravity to child
+
+				angularacceleration = body.poststepangvelocity - body.prestepangvelocity # Calculate angular acceleration of the parent
+				body.angular_velocity -= angularacceleration # Subtract angular acceleration from the child
+
+	def step(self, dt):
+		self.space.step(dt)
+	
+	def draw(self, screen):
+		for shape in self.shapes:
+			if not shape.hidden:
+				drawShape(screen, shape, shape.color, self.parentbody.position, self.parentbody.angle)
+	
+	def debugdraw(self, screen):
+		for body in self.bodies:
+			if body.body_type == pymunk.Body.DYNAMIC:
+				debugpos = body.position.rotated(self.parentbody.angle) + self.parentbody.position
+				debugposx, debugposy = debugpos
+				debugaccelposx, debugaccelposy = debugpos + body.acceleration.rotated(self.parentbody.angle) * 10
+				debugveladdposx, debugveladdposy = debugpos + body.velocityadd.rotated(self.parentbody.angle) * 10
+				pygame.draw.aaline(screen, (255, 0, 0), (debugposx, debugposy), (debugaccelposx, debugaccelposy))
+				pygame.draw.aaline(screen, (0, 255, 0), (debugposx, debugposy), (debugveladdposx, debugveladdposy))
+				
+
 def main():
 	debugdraw = False
 
@@ -41,14 +105,15 @@ def main():
 
 	mainspace.add(objectbody, objectbox)
 
-	objectspace = pymunk.Space()
-	objectspace.gravity = (0.0, 0.0)
-	objectenvironmentbody = pymunk.Body(body_type = pymunk.Body.STATIC)
-	objectenvironmentverts, objectenvironmentsegments = genWalls(objectenvironmentbody, screenwidth * .25, screenheight * .25, centered=True)
-	objectspace.add(objectenvironmentbody)
-	for segment in objectenvironmentsegments:
+	objectbody.localgrid = Localgrid(objectbody)
+
+	localgridenvironmentbody = pymunk.Body(body_type = pymunk.Body.STATIC)
+	localgridenvironmentverts, localgridenvironmentsegments = genWalls(localgridenvironmentbody, screenwidth * .25, screenheight * .25, centered=True)
+	objectbody.localgrid.addBodies(localgridenvironmentbody)
+	for segment in localgridenvironmentsegments:
 		segment.friction = 1
-		objectspace.add(segment)
+		segment.hidden = True
+		objectbody.localgrid.addShapes(segment)
 	
 	innerobjectbody = pymunk.Body(body_type = pymunk.Body.DYNAMIC)
 	innerobjectbody.position = pymunk.Vec2d(0, 0)
@@ -59,7 +124,17 @@ def main():
 	innerobjectbox.friction = 1
 	innerobjectbox.density = 1
 
-	objectspace.add(innerobjectbody, innerobjectbox)
+	objectbody.localgrid.addBodies(innerobjectbody)
+	objectbody.localgrid.addShapes(innerobjectbox)
+
+	for i in range(6):
+		circlebody = pymunk.Body(body_type = pymunk.Body.DYNAMIC)
+		circle = pymunk.Circle(circlebody, screenwidth * .1 * .25)
+		circle.elasticity = .1
+		circle.friction = 1
+		circle.density = 1
+		objectbody.localgrid.addBodies(circlebody)
+		objectbody.localgrid.addShapes(circle)
 
 	while True:
 		for event in pygame.event.get():
@@ -71,8 +146,7 @@ def main():
 						elif event.key == K_d:
 							debugdraw = not debugdraw
 
-		innerobjectbody.prestepvelocity = objectbody.velocity_at_local_point(innerobjectbody.position) # Record absolute velocity of the parent object at the location of the child object
-		innerobjectbody.prestepangvelocity = objectbody.angular_velocity # Record angular velocity of the parent object
+		objectbody.localgrid.prestep()
 
 		keyspressed = pygame.key.get_pressed()
 		if keyspressed[K_UP] or keyspressed[K_KP8]: fire_engine(objectbody, (0,-1)) # Up
@@ -84,33 +158,17 @@ def main():
 		
 		mainspace.step(1/framerate)
 
-		innerobjectbody.poststepvelocity = objectbody.velocity_at_local_point(innerobjectbody.position) # Record absolute velocity again after the timestep so that acceleration can be computed
-		innerobjectbody.poststepangvelocity = objectbody.angular_velocity # Record angular velocity again so that angular acceleration can be computed
-		
-		acceleration = (innerobjectbody.poststepvelocity - innerobjectbody. prestepvelocity).rotated(-objectbody.angle + math.radians(180)) # Calculate acceleration of the parent, rotate it by parent angle to translate it into child space, and flip it so that the child gets the opposite acceleration
-		velocityadd = acceleration + (mainspace.gravity.rotated(-objectbody.angle)/framerate) # Add gravity to the acceleration. If parent is in freefall, gravity on the parent and this gravity will cancel out, providing weightlessness.
-		innerobjectbody.velocity += velocityadd # Apply acceleration/gravity to child
+		objectbody.localgrid.poststep()
 
-		angularacceleration = innerobjectbody.poststepangvelocity - innerobjectbody.prestepangvelocity # Calculate angular acceleration of the parent
-		innerobjectbody.angular_velocity -= angularacceleration # Subtract angular acceleration from the child
+		objectbody.localgrid.step(1/framerate)
 
-
-		objectspace.step(1/framerate)
 		screen.fill((0, 0, 0))
 		pygame.gfxdraw.aapolygon(screen, environmentverts, (255, 255, 255)) # Draw Walls
-		drawPoly(screen, objectbox, (255, 255, 255), pymunk.Vec2d(0,0), 0) # Draw parent box
-		drawPoly(screen, innerobjectbox, (255, 255, 255), objectbox.body.position, objectbox.body.angle) # Draw child box. Gets corner of parent box and angle of parent box to offset child box to draw it inside the parent
+		drawShape(screen, objectbox, (255, 255, 255), pymunk.Vec2d(0,0), 0) # Draw parent box
+		objectbody.localgrid.draw(screen)
 
 		if debugdraw:
-			debugpos = innerobjectbox.body.position.rotated(objectbox.body.angle) + objectbox.body.position
-			debugposx, debugposy = debugpos
-			debugaccelposx, debugaccelposy = debugpos + acceleration.rotated(objectbox.body.angle) * 10
-			debugveladdposx, debugveladdposy = debugpos + velocityadd.rotated(objectbox.body.angle) * 10
-			pygame.draw.aaline(screen, (255, 0, 0), (debugposx, debugposy), (debugaccelposx, debugaccelposy))
-			pygame.draw.aaline(screen, (0, 255, 0), (debugposx, debugposy), (debugveladdposx, debugveladdposy))
-
-			centerx, centery = objectbox.body.position
-			pygame.gfxdraw.pixel(screen, int(centerx), int(centery), (255, 255, 0))
+			objectbody.localgrid.debugdraw(screen)
 
 		pygame.display.flip()
 
@@ -125,13 +183,19 @@ def fire_engine(objectbody, direction=(0,-1), force=None):
 			return
 		objectbody.apply_force_at_local_point((force * direction.x * objectbody.mass, force * direction.y * objectbody.mass))
 
-def drawPoly(screen=pygame.display, poly = pymunk.Poly, color = tuple, position = pymunk.vec2d, angle = float):
-	verts = list()
-	for v in poly.get_vertices():
-		v = v.rotated(poly.body.angle) + poly.body.position
-		x,y = v.rotated(angle) + position
-		verts.append((x, y))
-	pygame.gfxdraw.aapolygon(screen, verts, color)
+def drawShape(screen, shape, color, position, angle):
+	if type(shape) == pymunk.shapes.Poly:
+		verts = list()
+		for v in shape.get_vertices():
+			v = v.rotated(shape.body.angle) + shape.body.position
+			x,y = v.rotated(angle) + position
+			verts.append((x, y))
+		pygame.gfxdraw.aapolygon(screen, verts, color)
+	elif type(shape) == pymunk.shapes.Circle:
+		c = shape.offset.rotated(shape.body.angle) + shape.body.position
+		c = c.rotated(angle) + position
+		x, y = c
+		pygame.gfxdraw.aacircle(screen, int(x), int(y), int(shape.radius), color)
 
 def genWalls(body, width, height, centered = False):
 	verts = list()
